cfcdc4a0edc5e79cb6629f8f020c3e4ad6f26494
Vivek S. Lakhani <88651044+vivek-lakhani-1@users.noreply.github.com>
2023-10-06 10:21:44 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import GooglePalmEmbeddings
+from langchain.llms import GooglePalm
+from langchain.vectorstores import Pinecone
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+import pinecone
+import os
+from decouple import config
+import sys
+
+
+os.environ['GOOGLE_API_KEY'] = config("GOOGLE_API_KEY")
+embeddings=GooglePalmEmbeddings()
+PINECONE_API_KEY = config("PINECONE_API_KEY")
+PINECONE_API_ENV = config("PINECONE_API_ENV")
+
+pinecone.init(
+    api_key=PINECONE_API_KEY,
+    environment=PINECONE_API_ENV 
+)
+index_name = "amitava"
+
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
+
+
+llm = GooglePalm(temperature=0.1)
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever())
+
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def getting_data():
+    data = request.json
+    query = data.get('query', 'World')
+    
+    response_data = {
+        'message': qa({'query': query})['result'],
+    }
+
+    return jsonify(response_data), 200
+
+if __name__ == '__main__':
+    app.run()
